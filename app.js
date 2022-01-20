4e4100fc7a01eec446bf38772675f0c954910060
Yu Jia <rockjia1996@gmail.com>
2022-01-19 19:06:40 -0500

    Finish the prototype server that is written with Express, Add the documentation for it.

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const ExpressSimpleHttpServer = require("./ExpressSimpleHttpServer");
 
 const server = new ExpressSimpleHttpServer(8000);
 
+server.cacheDefaultData();
+server.loadDefaultRoutes();
 server.start();
